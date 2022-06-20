fixed signs on water quality parameters

objective_gradient.py
@@ -217,8 +217,7 @@ def objective_grad(strategy, n, l, N, K, M, R, tot,
   grad_P_minus[1] += de2_de1*grad_P_minus[0]
   grad_P_plus[1] += de2_de1*grad_P_plus[0]
   
-  # average objectives across resources to get an aggregated objective. average is weighted by sigma_tildes, an indication of importance of 
-  # each resource to the user
+  # average objectives across resources to get an aggregated objective. average is weighted by sigma_tildes, an indication of #importance of each resource to the user
   grad_G_avg = np.sum(sigma_tildes[:,l][:,np.newaxis,np.newaxis]*grad_G, axis=0)
   grad_E_avg = np.sum(sigma_tildes[:,l][:,np.newaxis]*grad_E, axis=0)
   grad_T_avg = np.sum(sigma_tildes[:,l]*grad_T, axis=0)

run_gen_model.py
@@ -180,7 +180,9 @@ def set_fixed_exp_params(N, M, K,N_list,M_list,K_list,tot,R):
   # 2: small district farmers
   # 3: investor district farmers
   # 4: small white area growers
-  # 5: investor white area growers
+  # 5: investor white area growers 
+  
+  # TO DO: fix parameterization for water quality!!!
   sw_users = np.array([False]*(N+K))
   sw_users[1:3] = True
   ds_dr = np.zeros((2))
@@ -191,7 +193,7 @@ def set_fixed_exp_params(N, M, K,N_list,M_list,K_list,tot,R):
   de_dr[1,2] = np.random.uniform(0,0.5)
   de_dr[1,3] = np.random.uniform(1,2)
   de_dr[1,4] = np.random.uniform(0,0.5)
-  de_dr[2,0] = np.random.uniform(1,2)
+  de_dr[2,0] = np.random.uniform(1,2)*-1
   dt_dr = 0.5 
   de2_de1 = -1
   de_dg = np.zeros((3,M,N))  ###### $
@@ -315,7 +317,7 @@ def run_system(user = None):
       final system
     The remaining outputs are all of the sampled or computed scale, exponent, and strategy parameters.
   '''
-
+  print('b')
   entities = pd.read_excel('parameter_files\entity_list.xlsx',sheet_name=None, header=None)
   N_list=entities['N'].values[:,0]
   N = len(N_list)
@@ -346,7 +348,7 @@ def run_system(user = None):
     stability_1 = False  # unstable if real part is positive, inconclusive if 0
   
   if user != None:
-    max_iters = 100  # change back to 100!! 
+    max_iters = 100 # change back to 100!! 
     strategy, stability_2, stability_3 = optimize_strategy(max_iters, user, N, K, M, tot, R,
       phis, psis, psi_bars, eq_R_ratio, psi_tildes, alphas, beta_tildes, sigma_tildes, betas, beta_hats, beta_bars, sigmas, sigma_hats, etas, eta_bars, eta_hats, lambdas, lambda_hats, G, E, T, H, C, P, ds_dr, de_dr, dt_dr, de2_de1, de_dg, de_dE, dg_dG, dh_dH, dg_dy, dh_dy, dt_dh, dt_dT, db_de, dc_dC, dp_dP, dp_dy, du_dx_plus, du_dx_minus)
   else:

strategy_interpretation.py
@@ -25,7 +25,7 @@ resource_user = 'rural communities'
 #resource_user = 'investor growers'
 #resource_user = 'investor growers (white area)'
 
-with open('strategies_%s_0'%(resource_user), 'rb') as f:
+with open('strategies_%s_looping'%(resource_user), 'rb') as f:
   strategies = pickle.load(f)  
  
 strategies[np.abs(strategies)<0.0015] = 0